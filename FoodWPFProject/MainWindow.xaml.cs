10538d494ff84c68e71006467d02df07df69c8d1
GalZisin <46171488+GalZisin@users.noreply.github.com>
2019-09-22 04:49:32 +0300

    Add files via upload

---
+++ FoodWPFProject/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using FoodProject;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FoodWPFProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+      
+      
+
+        private const string URL = "http://localhost:53926/api/Food";
+        FoodDAOMSSQL foodDao = null;
+        List<Food> foods = new List<Food>();
+        public MainWindow()
+        {
+            foodDao = new FoodDAOMSSQL();
+            InitializeComponent();
+            foods = foodDao.GetAllFoods();
+            grid.ItemsSource = foods;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foods = foodDao.GetAllFoods();
+            grid.ItemsSource = foods;
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            ViewAddFood viewAddFood = new ViewAddFood();
+            viewAddFood.Show();
+        }
+    }
+}
+
---
+++ FoodWPFProject/ViewAddFood.xaml.cs
@@ -0,0 +1,43 @@
+﻿using FoodProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FoodWPFProject
+{
+    /// <summary>
+    /// Interaction logic for ViewAddFood.xaml
+    /// </summary>
+    public partial class ViewAddFood : Window
+    {
+        FoodDAOMSSQL foodDao = null;
+        public ViewAddFood()
+        {
+            foodDao = new FoodDAOMSSQL();
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Food food = new Food
+            {
+                Name = name.Text,
+                Calories = Convert.ToInt32(calories.Text),
+                Grade = Convert.ToInt32(grade.Text),
+                Ingridients = ingredients.Text
+            };
+
+            foodDao.AddFood(food);
+        }
+    }
+}
